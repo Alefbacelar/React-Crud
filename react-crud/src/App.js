2f354aa6e9ec0ad45a0b8559f95a139883067da3
ALEF BACELAR <74435383+Alefbacelar@users.noreply.github.com>
2023-01-11 20:54:05 -0300

    Create done.

--- react-crud/src/App.js
+++ react-crud/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Create from './components/create';
 
 function App() {
   return (
     <div className='main'>
       <h2 className='main-header'>React Crud Operations</h2>
+      <div>
+        <Create/>
+      </div>
     </div>
   );
 }
---
+++ react-crud/src/components/create.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Button, Checkbox, Form } from 'semantic-ui-react'
+
+export default function Create() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [checkbox, setCheckbox] = useState(false);
+    const postData = () => {
+      console.log(firstName);
+      console.log(lastName);
+      console.log(checkbox);
+}
+    return (
+        <div>
+            <Form className="create-form">
+                <Form.Field>
+                    <label>First Name</label>
+                    <input placeholder='First Name' onChange={(e) => setFirstName(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Last Name</label>
+                    <input placeholder='Last Name' onChange={(e) => setLastName(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                <Checkbox label='I agree to the Terms and Conditions' onChange={(e) => setCheckbox(!checkbox)}/>
+                </Form.Field>
+                <Button onClick={postData} type='submit'>Submit</Button>
+            </Form>
+        </div>
+    )
+}
